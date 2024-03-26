e4a1d0212af6d07c4a32d85613747aeb9e51ff4b
Deepa-Lakshmi-Ravi <deviravichandren02@gmail.com>
2024-03-26 12:38:44 -0700

    name changed in router

--- routers/User.router.js
+++ routers/User.router.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const UserController = require("../Controllers/User.controller");
-const Validators = require("../Public/Validator");
+//const Validators = require("../Public/Validator");
 
 const router = express.Router();
 
 router.post("/signup", UserController.Signup);
-router.post("/signin", UserController.Signin);
+router.post("/login", UserController.Signin);
 router.post("/forget-password", UserController.forgetPassword);
 router.post("/reset-password", UserController.resetPassword);
 

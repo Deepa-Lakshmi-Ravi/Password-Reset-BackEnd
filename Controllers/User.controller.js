3e9ccf50aaba3fe919286567e7f7d1af1cb3fdeb
Deepa-Lakshmi-Ravi <deviravichandren02@gmail.com>
2024-03-27 11:24:46 -0700

    added to the changes

--- Controllers/User.controller.js
+++ Controllers/User.controller.js
@@ -39,32 +39,46 @@ const Signup = async (req, res) => {
 
 const Signin = async (req, res) => {
   try {
-    const { email, password } = req.body;
-    const user = await userModel.findOne({ email });
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    const isPasswordValid = await auth.hashCompare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: "Invalid password" });
-    }
-    const token = await auth.createToken({
-      id: user._id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      email: user.email,
-      role: user.role,
-    });
-    let userData = await userModel.findOne(
-      { email: req.body.email },
-      { _id: 0, password: 0, createdAt: 0, email: 0 }
-    );
-    res.status(200).json({ message: "Signin successful", token, userData });
+      let user = await userModel.findOne({ email: req.body.email });
+      if (user) {
+          let hashCompare = await auth.hashCompare(req.body.password, user.password);
+          if (hashCompare) {
+              const token = await auth.createToken({
+                  id: user._id,
+                  email: user.email,
+                  role: user.role
+              });
+
+              // Send the response with the token
+              res.status(200).send({
+                  message: `User ${user.userName} is logged in successfully`,
+                  token,
+                  user: {
+                      _id: user._id,
+                      firstName: user.firstName,
+                      lastName: user.lastName,
+                      email: user.email,
+                      role: user.role
+                  }
+              });
+          } else {
+              res.status(401).send({
+                  message: "Invalid Password"
+              });
+          }
+      } else {
+          res.status(404).send({
+              message: `User with ${req.body.email} is not found. Please sign up.`
+          });
+      }
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
+      res.status(500).send({
+          message: "Internal Server Error",
+          error: error.message
+      });
   }
 };
+
 
 const forgetPassword = async (req, res) => {
   try {

af6ae5b1d39f4f282e18d6f720661b1915ca99c2
Deepa-Lakshmi-Ravi <deviravichandren02@gmail.com>
2024-03-28 17:11:20 -0700

    added to changes

--- routers/User.router.js
+++ routers/User.router.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const UserController = require("../Controllers/User.controller");
-//const Validators = require("../Public/Validator");
 
 const router = express.Router();
 
-router.post("/signup", UserController.Signup);
+router.post("/signup",UserController.Signup);
 router.post("/login", UserController.Signin);
-router.post("/forget-password", UserController.forgetPassword);
+router.post("/forget-password",UserController.forgetPassword);
 router.post("/reset-password/:randomString/:expirationTimestamp", UserController.resetPassword);
 
 module.exports = router;
